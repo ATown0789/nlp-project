788d9127f697c994479015eb1436b38013d40f2b
Anthony Collini <collini.anthony@gmail.com>
2020-07-12 16:27:37 -0500

    feat: Add simple jest test

---
+++ _test_/URLSubmit.test.js
@@ -0,0 +1,11 @@
+//import file to test
+import { URLSubmit } from '../src/client/js/URLSubmit.js'
+
+
+describe("Testing the submit functionality", () => { 
+			test("Testing the URLSubmit() function is defined", () => {
+				expect(URLSubmit).toBeDefined();
+			})
+			
+			
+	});
