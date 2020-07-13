cf162863af01d51f21d1db61ae051025ad9e8edd
Anthony Collini <collini.anthony@gmail.com>
2020-07-12 20:21:10 -0500

    chore: clean up trailing space

--- _test_/URLSubmit.test.js
+++ _test_/URLSubmit.test.js
@@ -2,10 +2,8 @@
 import { URLSubmit } from '../src/client/js/URLSubmit.js'
 
 
-describe("Testing the submit functionality", () => { 
+describe("Testing the submit functionality", () => {
 			test("Testing the URLSubmit() function is defined", () => {
 				expect(URLSubmit).toBeDefined();
 			})
-			
-			
 	});
